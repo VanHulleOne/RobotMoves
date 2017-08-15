4d9edaf43445360313974c9372633575f6f2d5a5
ENGR\cbrueggeman <cbrueggeman@wisc.edu>
2017-08-15 12:30:39 -0500

    Made set feed rate its own function

--- quaternions.py
+++ quaternions.py
@@ -153,13 +153,7 @@ def outsideCylinder(*, centerX=0, centerY=0, centerZ=15, dia=16.8, length=None,
     currHeight = centerZ
     
     if (setFeedRate == True):
-        feedRateWaitTime = ((vel*(28.216/30))+.5)/5.6154                           #28.216/30 is from the relation of 25 mm/min feed rate for a speed of 30 mm/s, the rest was found from a linear fit
-        yield('\t\tSetDO DO6_Between_Layer_Retract, 1;\n')
-        yield('\t\tWaitTime .1;\n')
-        yield('\t\tSetDO DO5_Program_Feed, 1;\n')
-        yield('\t\tWaitTime ' + str(feedRateWaitTime) + ';\n')
-        yield('\t\tSetDO DO5_Program_Feed, 0;\n')
-        yield('\t\tSetDO DO6_Between_Layer_Retract, 0;\n\n')
+        yield from setFeedRate(vel = vel)
     
     yield('\t\tSetDO DO1_Auto_Mode, 1;\n')
     
@@ -340,13 +334,8 @@ def helix(diameter = 7.0, stepsPerRev = 4.0, height= 95.0, layerHeight = .2,
     thetaStep = 0
     thetaTotal = 0
     
-    feedRateWaitTime = ((vel*(28.216/30))+.5)/5.6154                           #28.216/30 is from the relation of 25 mm/min feed rate for a speed of 30 mm/s, the rest was found from a linear fit
-    yield('\t\tSetDO DO6_Between_Layer_Retract, 1;\n')
-    yield('\t\tWaitTime .1;\n')
-    yield('\t\tSetDO DO5_Program_Feed, 1;\n')
-    yield('\t\tWaitTime ' + str(feedRateWaitTime) + ';\n')
-    yield('\t\tSetDO DO5_Program_Feed, 0;\n')
-    yield('\t\tSetDO DO6_Between_Layer_Retract, 0;\n\n')
+    yield from setFeedRate(vel = vel)
+    
     yield('\t\tSetDO DO1_Auto_Mode, 1;\n')
     
     yield('\t\tSetDO DO4_Heat_Nozzle, 1;\n')
@@ -587,7 +576,26 @@ def setTemp(bedTemp = 0, nozzleTemp = 220, setBedTemp = False, setNozzleTemp = F
         yield('\t\tWaitTime ' + str(nozzleTempWaitTime) + ';\n')
         yield('\t\tSetDO DO5_Program_Feed, 0;\n')
         
-    yield('\t\tSetDO DO6_Between_Layer_Retract, 0;\n')    
+    yield('\t\tSetDO DO6_Between_Layer_Retract, 0;\n') 
+    
+def setFeedRate(vel = 10):
+    if vel < 0:
+        raise Exception('Temperature cannot be negative')
+        
+    feedRateWaitTime = ((vel*(28.216/30))+.5)/5.6154                           #28.216/30 is from the relation of 25 mm/min feed rate for a speed of 30 mm/s, the rest was found from a linear fit
+    yield('\t\tSetDO DO6_Between_Layer_Retract, 1;\n')
+    yield('\t\tWaitTime .1;\n\n')
+
+    yield('\t\tSetDO DO1_Auto_Mode, 1;\n')
+    yield('\t\tWaitTime .1;\n\n')    
+  
+    yield('\t\tSetDO DO1_Auto_Mode, 0;\n')
+    
+    yield('\t\tSetDO DO5_Program_Feed, 1;\n')
+    yield('\t\tWaitTime ' + str(feedRateWaitTime) + ';\n')
+    yield('\t\tSetDO DO5_Program_Feed, 0;\n')
+    
+    yield('\t\tSetDO DO6_Between_Layer_Retract, 0;\n\n')
 
         
 def writePoints(points):
